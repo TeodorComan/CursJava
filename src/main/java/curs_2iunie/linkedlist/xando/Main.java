cf222c0e77ea396bc16effad030e90dae17b8d4d
Teodor Coman <teodor.coman@mindit.ro>
2018-06-10 09:13:17 +0300

    main class for dynamic linked board

--- src/main/java/curs_2iunie/linkedlist/xando/Main.java
+++ src/main/java/curs_2iunie/linkedlist/xando/Main.java
@@ -4,21 +4,7 @@ public class Main {
 
     public static void main( String[] args ) {
 
-        Board board = new Board();
+        Board board = new Board(3,3);
 
-        Cell cell2a= new Cell();
-        cell2a.setLeft(board.getTopLeft());
-        board.getTopLeft().setRight(cell2a);
-
-        Cell cell3a = new Cell();
-        cell3a.setLeft(cell2a);
-        cell2a.setRight(cell3a);
-
-        Cell cell1b = new Cell();
-        cell1b.setTop(board.getTopLeft());
-        board.getTopLeft().setBottom(cell1b);
-
-        Cell cell2b = new Cell(cell1b,null,cell2a,null);
-        Cell cell3b = new Cell(cell2b,null,cell3a,null);
     }
 }
